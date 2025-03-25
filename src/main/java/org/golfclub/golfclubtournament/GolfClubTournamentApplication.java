e4ba477e08af5e33a76d0b3d64c61c01174e4236
Beth-Ann <bethann.penney@gmail.com>
2025-03-25 20:51:00 -0230

    Initial commit

---
+++ src/main/java/org/golfclub/golfclubtournament/GolfClubTournamentApplication.java
@@ -0,0 +1,13 @@
+package org.golfclub.golfclubtournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfClubTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/golfclub/golfclubtournament/GolfClubTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.golfclub.golfclubtournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfClubTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
