267f795f55d71a0633735041f99f2064443acbf1
Beth-Ann <bethann.penney@gmail.com>
2025-03-26 18:07:26 -0230

    Added code to Member and Tournament repository files - Fixed more files

--- src/main/java/org/golfclub/golfclubtournament/repository/MemberRepository.java
+++ src/main/java/org/golfclub/golfclubtournament/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package org.golfclub.golfclubtournament.repository;
 
-public class MemberRepository {
+import org.golfclub.golfclubtournament.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByNameContainingIgnoreCase(String name);
+    List<Member> findByPhoneNum(String phoneNum);
+    List<Member> findByStartDate(LocalDate startDate);
+    List<Member> findByTournaments_StartDate(LocalDate tournamentStartDate);
 }
+
